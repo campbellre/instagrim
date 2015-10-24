4220becac87e3c4b2af6077fbc5ff5bc6ab927ab
campbellre <r.e.campbell@dundee.ac.uk>
2015-10-24 15:21:45 +0100

    Create a view image page, and have is showing a image from uuid.

---
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/ViewImage.java
@@ -0,0 +1,54 @@
+package uk.ac.dundee.computing.aec.instagrim.servlets;
+
+import com.datastax.driver.core.Cluster;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import uk.ac.dundee.computing.aec.instagrim.lib.CassandraHosts;
+import uk.ac.dundee.computing.aec.instagrim.lib.Convertors;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Ryan on 24/10/2015.
+ *
+ *  Servlet for displaying an image and the details about it. e.g. comments.
+ */
+@WebServlet(name = "ViewImage", urlPatterns = "/ViewImage/*")
+public class ViewImage extends HttpServlet {
+
+    Cluster cluster = null;
+
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+        cluster = CassandraHosts.getCluster();
+    }
+
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        // nothing here yer
+//        throw new NotImplementedException();
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String args[] = Convertors.SplitRequestPath(request);
+
+        request.setAttribute("image_url", args[2]);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/ViewImage.jsp");
+        rd.forward(request,response);
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/ViewImage.jsp
@@ -0,0 +1,14 @@
+<%--
+  User: Ryan
+  Date: 24/10/2015
+  Time: 15:06
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>></title>
+</head>
+<body>
+    <img src="/Instagrim/Image/<%=request.getAttribute("image_url")%>" />
+</body>
+</html>
