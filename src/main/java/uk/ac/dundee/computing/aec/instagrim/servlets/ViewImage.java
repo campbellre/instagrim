99a6910f75aac59be9e685da64ee3679930e6606
campbellre <r.e.campbell@dundee.ac.uk>
2015-10-24 16:07:10 +0100

    Added post code for entering a comment.

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/ViewImage.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/ViewImage.java
@@ -4,6 +4,8 @@ import com.datastax.driver.core.Cluster;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import uk.ac.dundee.computing.aec.instagrim.lib.CassandraHosts;
 import uk.ac.dundee.computing.aec.instagrim.lib.Convertors;
+import uk.ac.dundee.computing.aec.instagrim.models.Comment;
+import uk.ac.dundee.computing.aec.instagrim.stores.LoggedIn;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -12,6 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -31,8 +34,30 @@ public class ViewImage extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        HttpSession session = request.getSession();
+        LoggedIn lg = (LoggedIn) session.getAttribute("LoggedIn");
+        if(lg == null)
+        {
+            // Error message
+        }
+        else {
+            if(lg.getlogedin()) {
+                String user = lg.getUsername();
+                String commentBody = request.getParameter("CommentBody");
+                String args[] = Convertors.SplitRequestPath(request);
 
+                Comment c = new Comment();
 
+                c.EnterComment(user,commentBody,args[2]);
+
+            }
+            else{
+                // Error message
+            }
+
+        }
+        RequestDispatcher rd = request.getRequestDispatcher("/ViewImage.jsp");
+        rd.forward(request,response);
 
     }
 
